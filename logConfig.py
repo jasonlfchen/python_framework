ea4b8809b9083f6d6727f57394f866c2b93390ba
unknown <JMR947@N840GXLZXUHZCES.COFPROD.COF>
2015-02-17 14:22:40 -0600

    add logger

---
+++ logConfig.py
@@ -0,0 +1,13 @@
+import logging
+import logging.config
+
+# get logger configration
+logging.config.fileConfig('logging.ini')
+# create logger
+logger = logging.getLogger('fileLog')
+
+# sample loggers
+logger.info('info test message')
+logger.warn('warn test message')
+logger.error('error test message')
+logger.critical('critical test message')
