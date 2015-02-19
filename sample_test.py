8ece17bf76143eb39e94d11c18c704fe8aab873a
JC <jchen7960@gmail.com>
2015-02-19 04:48:29 -0600

    Added sample html reporter, modified logger output, added logger to CommonMethods, added html files to gitignore

--- sample_test.py
+++ sample_test.py
@@ -1,30 +1,43 @@
-_name_ = 'jason'
+_author_ = 'jason'
 
 import os
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from common.CommonMethods import CommonMethods
+import logging
+import logging.config
 
-cm = CommonMethods()
-#cm.set_browser_path('chrome','/Users/Jason/PycharmProjects/driver/chromedriver') #on mac
-cm.open_browser('firefox')
-cm.navigate("https://qa4.ca.capitalonecardservice.btctest.com/ecare/loginform")
-print("on eCare site")
-cm.assert_title("Online Account Access Login")
-driver = cm.get_driver()
-#assert "Online Account Access Login" in driver.title
-cm.set_value_to_element('CO7748U','id','userid1')
-cm.set_value_to_element('COACCOUNT1','id','password1')
-#elem = driver.find_element_by_id("userid1")
-#elem.send_keys("CO7748U")
-#elem = driver.find_element_by_id("password1")
-#elem.send_keys("COACCOUNT1")
-cm.click(driver,'name','button1')
-#driver.find_element_by_name("button1").click()
-print("entered credentials")
-cm.assert_title("Account Overview")
-print("test passed")
-cm.close_browser()
+login_btn = ['name','button1']
+usr_name_field = [By.ID,'userid1']
+password_field = ['id','password1']
+
+class EcareTests(unittest.TestCase):
+
+    def set_Up(self):
+        self.logging.config.fileConfig('logging.ini')
+        self.logger = logging.getLogger('fileLog')
+        self.cm = CommonMethods()
+        #cm.set_browser_path('chrome','/Users/Jason/PycharmProjects/driver/chromedriver') #on mac
+        self.cm.open_browser('firefox')
+        self.cm.navigate("https://qa4.ca.capitalonecardservice.btctest.com/ecare/loginform")
+        self.logger.info("on eCare site")
+        self.cm.assert_title("Online Account Access Login")
+
+    def login_test(self):
+        self.driver = self.cm.get_driver()
+        self.cm.set_value_to_element('CO7748U',*usr_name_field)
+        self.cm.set_value_to_element('COACCOUNT1',*password_field)
+        self.cm.click(driver,*login_btn)
+        self.logger.info("entered credentials")
+        self.cm.assert_title("Account Overview")
+        self.logger.info("test passed")
+
+    def tear_down(self):
+        self.cm.close_browser()
+
+if __name__ == "__main__":
+    unittest.main()
